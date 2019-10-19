3038d626e4f4c25aa00473e360788b174b9cc496
Nagarajcruze <32562272+nagarajcruze@users.noreply.github.com>
2019-10-19 12:35:38 +0530

    Update Capture_Images.py

--- Capture_Images.py
+++ Capture_Images.py
@@ -5,10 +5,10 @@ count = 1
 
 face_id = input("enter your name: ")
 
-for _ in range(10):
+for _ in range(10):#change this 10 to any other number based on how many images that you want
     ret ,image = cap.read()
     cv2.imshow('frame', image)
-    cv2.imwrite("pics/"+face_id+"_%d.jpg" % count, image)
+    cv2.imwrite("pictures/"+face_id+"_%d.jpg" % count, image)
     print("%d --> successfull "%count)
     count +=1
     if cv2.waitKey(5) & 0xFF == ord('q'):
